d6703662cb443111485ef2bbf237b2071a505e25
Adam McKnight <adammcknight@me.com>
2019-03-27 14:40:53 -0400

    Finished Logic,and Added a Fav Icon.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,5 @@
-
 // Makes Weather API Call...
-const apiKey = `https://api.openweathermap.org/data/2.5/weather?zip=45044,us&appid=5a0b396cf9576b707a085b16a204ac46`;
+    const apiKey = `https://api.openweathermap.org/data/2.5/weather?zip=45044,us&appid=5a0b396cf9576b707a085b16a204ac46`;
 
 $.getJSON(apiKey, function(apiData){
     console.log(apiData)
@@ -15,17 +14,23 @@ $.getJSON(apiKey, function(apiData){
     let city = apiData.name;
 
     // Adds the tempature in Fahrenheit to the DOM...
-    $("#data").append("<h2>" + "It's currently " + tempF + " degrees Fahrenheit " + "in " + city + "!" + "</h2>");
-
-
+    $("#data").append("<h2>" + "It's currently " + "<span class='large'>" + tempF + "</span>" + " degrees Fahrenheit " + "in " + city + "!" + "</h2>");
 
     // Logic for temp sayings...
     if(tempF >= 75){
         $("#saying").append("<h2>" + "Dang! That's really friggin' hawt! 🔥" + "</h2>");
+        $("html").css("background-color", "red");
     } else if(tempF <= 40) {
         $("#saying").append("<h2>" + "Holy Hockeysticks! That's really cold... Brrr. 🏂" + "</h2>");
+        $("html").css("background-color", "blue")        
+        
     } else{
         $("#saying").append("<h2>" + "Is it Cold, is it Hot? Nobody Knows. Or Cares!" + "</h2>");
+        $("html").css("background-color", "white");
+        $("body > .temperature").css("color", "black");
     }
 });
+
+
+
 
